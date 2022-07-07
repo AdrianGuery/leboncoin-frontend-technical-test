2b7317ff1f6469b02d274dcb6f714617054a757e
Adrian Guery <adrian.guery@toolpad.fr>
2022-07-07 08:44:06 +0200

    Install axios & react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,22 @@
 import type { AppProps } from 'next/app'
 import { getLoggedUserId } from '../utils/getLoggedUserId'
 import '../styles/globals.css'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
 
 // Default way to get a logged user
 export const loggedUserId = getLoggedUserId()
 
+const queryClient = new QueryClient()
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
 }
 
 export default MyApp
